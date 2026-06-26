a94787a50aeed7b6879de92058ede08f7f97038a
mirazizku13 <mirazizmirabdulayev11@gmail.com>
2026-06-26 18:12:21 +0500

    drf-4: Token authentication qo‘shildi

--- DjangoProjectAPInormatov/settings.py
+++ DjangoProjectAPInormatov/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 3rd
     'rest_framework',
+    'rest_framework.authtoken',
 
 #     local
     'tasks',
@@ -130,5 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
  'DEFAULT_AUTHENTICATION_CLASSES': [
+    'rest_framework.authentication.TokenAuthentication',
  ]
 }
+
